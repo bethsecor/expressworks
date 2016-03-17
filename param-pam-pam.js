9d2d1d243d0d58315fd462adb34e1ae69b1a6416
Beth Secor <elizabeth.a.secor@gmail.com>
2016-03-16 18:42:21 -0600

    Complete expressworks tutorial

---
+++ param-pam-pam.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var bodyparser = require('body-parser')
+var app = express()
+app.use(bodyparser.urlencoded({extended: false}))
+app.put('/message/:id', function(req, res) {
+  var hash = require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + req.params.id)
+      .digest('hex')
+  res.send(hash)
+})
+app.listen(process.argv[2])
